56b62a7c669d208b7cee2267217a8a28571a41e3
woelmoe <nikskilfly@gmail.com>
2021-06-14 19:23:40 +0300

    camera
    test camera web-interface

---
+++ vuessr1/README.md
@@ -0,0 +1,29 @@
+# Vue-ssr-express
+
+## Project setup
+```
+npm install
+```
+## Project update
+```
+npm update
+```
+
+## Animate.css setup
+```
+npm install animate.css --save
+```
+
+### Compiles and hot-reloads for development
+```
+npm run dev
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### JSON server start
+```
+json-server --watch db.json
---
+++ vuessr1/webpack.base.config.js
@@ -0,0 +1,67 @@
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const VueLoaderPlugin = require( 'vue-loader/lib/plugin' );
+const autoprefixer = require( 'autoprefixer' );
+
+const IS_DEV = process.env.NODE_ENV === 'development';
+// const IS_PROD = process.env.NODE_ENV === 'production';
+
+console.log( "common config" );
+module.exports = {
+	mode: process.env.NODE_ENV,
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+            },
+			{
+				test: /\.js$/,
+				loader: "babel-loader",
+				exclude: /node_modules/,
+				options: {
+					sourceMap: IS_DEV
+				}
+            },
+			{
+				test: /\.s?css$/,
+				use: [
+					{
+						loader: 'vue-style-loader',
+						options: {
+							sourceMap: IS_DEV
+						}
+                    },
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: IS_DEV
+						}
+                    },
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: () => [ autoprefixer ]
+						}
+                    },
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: IS_DEV
+						}
+                    },
+                ],
+            },
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+					},
+				],
+			},
+    ],
+	},
+	plugins: [
+        new VueLoaderPlugin()
+    ]
+}
